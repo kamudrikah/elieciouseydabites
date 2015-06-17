f6f93dd1d89d513f713348aa433c55c7e9c9292a
Ku Ahmad Mudrikah <kamudrikah@gmail.com>
2015-06-18 01:36:20 +0800

    db connection change

--- controller/db_connect.php
+++ controller/db_connect.php
@@ -1,6 +1,12 @@
 <?php
 
+// $conn = mysqli_connect("localhost","u972940147_boos","admin123","u972940147_boos");
 $conn = mysqli_connect("127.0.0.1","root","","u972940147_boos");
+
+// $host = "localhost";
+// $db_name = "u972940147_boos";
+// $username = "u972940147_boos";
+// $password = "admin123";
 
 $host = "127.0.0.1";
 $db_name = "u972940147_boos";
--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,17 @@
 <?php
 
-$conn = mysqli_connect("localhost","u972940147_boos","admin123","u972940147_boos");
+// $conn = mysqli_connect("localhost","u972940147_boos","admin123","u972940147_boos");
+$conn = mysqli_connect("127.0.0.1","root","","u972940147_boos");
 
-$host = "localhost";
+// $host = "localhost";
+// $db_name = "u972940147_boos";
+// $username = "u972940147_boos";
+// $password = "admin123";
+
+$host = "127.0.0.1";
 $db_name = "u972940147_boos";
-$username = "u972940147_boos";
-$password = "admin123";
+$username = "root";
+$password = "";
 
 try {
 	$con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
